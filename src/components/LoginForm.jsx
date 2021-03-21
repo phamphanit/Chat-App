f9a863984877014520f8d718804f353f452933bf
phamphan <phamphanthsp@gmail.com>
2021-03-21 21:21:06 +0700

    test

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -38,7 +38,7 @@ const LoginForm = () => {
             required
           />
           <input
-            type="text"
+            type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             className="input"
